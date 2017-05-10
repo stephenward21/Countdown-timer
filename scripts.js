3db8f7f0dedfc9fdaa12456336936d51ab33e1cc
Stephen Ward <stephenward@Stephens-MacBook-Pro.local>
2017-05-10 16:15:02 -0400

    July 4th Countdown

---
+++ scripts.js
@@ -0,0 +1,44 @@
+function Timer(id, endtime){
+	this.endtime = endtime;
+	this.id = id;
+	var clock = document.getElementById(id);
+	this.weeksSpan = clock.querySelector('.weeks');
+	this.daysSpan = clock.querySelector('.days');
+	this.hoursSpan = clock.querySelector('.hours');
+	this.minutesSpan = clock.querySelector('.minutes');
+	this.secondsSpan = clock.querySelector('.seconds');
+}
+
+Timer.prototype.getTimeRemaining = function(){
+	var t = Date.parse(this.endtime) - Date.parse(new Date());
+	this.seconds = Math.floor((t / 1000) % 60);
+	this.minutes = Math.floor((t / 1000 / 60) % 60);
+	this.hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	this.days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
+	this.weeks = Math.floor(t / (1000 * 60 * 60 * 24 * 7));
+}
+
+Timer.prototype.updateTimer = function(){
+	this.getTimeRemaining();
+	this.weeksSpan.innerHTML = this.weeks;
+	this.daysSpan.innerHTML = this.days;
+	this.hoursSpan.innerHTML = this.hours;
+	this.minutesSpan.innerHTML = this.minutes;
+	this.secondsSpan.innerHTML = this.seconds;
+
+
+}
+
+// var endDate = new Date();
+var endDate = new Date(Date.parse('July 4, 2017'));
+// console.log(endDate);
+
+var fourthOfJulyTimer = new Timer('timer-div', endDate);
+setInterval(
+	function(){
+		fourthOfJulyTimer.updateTimer();
+	}, 1000
+
+)
+
+	
